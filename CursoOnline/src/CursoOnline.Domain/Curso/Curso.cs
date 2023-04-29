99fe8ff94b43461c2be9deb5ee4138cc943407b8
janainaon1 <janainaon1@gmail.com>
2023-04-29 10:58:58 -0300

    feat: implements CriacaoDoCurso test

---
+++ CursoOnline/src/CursoOnline.Domain/Curso/Curso.cs
@@ -0,0 +1,18 @@
+﻿namespace CursoOnline.Domain
+{
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public decimal Valor { get; private set; }
+
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, decimal valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+    }
+}
---
+++ CursoOnline/test/CursoOnline.Domain.Test/Cursos/CursoTest.cs
@@ -0,0 +1,24 @@
+﻿using CursoOnline.Domain;
+using Xunit;
+
+namespace CursoOnline.Domain.Test.Cursos
+{
+    public class CursoTest
+    {
+        [Fact(DisplayName = "CriacaoDoCurso")]
+        public void CriarCurso()
+        {
+            string nome = "Curso de TDD";
+            double cargaHoraria = 80;
+            string publicoAlvo = "Estudante";
+            decimal valor = 100;
+
+            var cursoParaCriar = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, cursoParaCriar.Nome);
+            Assert.Equal(cargaHoraria, cursoParaCriar.CargaHoraria);
+            Assert.Equal(publicoAlvo, cursoParaCriar.PublicoAlvo);
+            Assert.Equal(valor, cursoParaCriar.Valor);
+        }
+    }
+}
